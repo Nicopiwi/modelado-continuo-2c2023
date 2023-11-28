9edf70b607f27b7e399aab046375d39dfa017c54
Nicopiwi <nikorozenberg@gmail.com>
2023-11-28 01:06:02 -0300

    starting with tp3

---
+++ TP3/src/TP3.jl
@@ -0,0 +1,11 @@
+using LinearAlgebra
+using SparseArrays
+using BenchmarkTools
+
+#TODO: Crear Notebook utilizando estas funciones
+
+function metodo_explicito(h, dt)
+end
+
+function metodo_implicito()
+end
