a658058d7583149cadb81404a68e091df6908fbc
Nicopiwi <nikorozenberg@gmail.com>
2023-12-04 20:42:56 -0300

    add 2d method

--- TP3/src/TP3.jl
+++ TP3/src/TP3.jl
@@ -25,6 +25,7 @@ function metodo_explicito(tf, h, dt, alpha)
     tf: Tiempo final
     h: Paso en el espacio
     dt: Paso en el tiempo
+    alpha: Constante de difusividad
     """
 
     r = dt * alpha / (h^2)
@@ -57,6 +58,7 @@ function metodo_implicito(tf, h, dt, alpha)
     tf: Tiempo final
     h: Paso en el espacio
     dt: Paso en el tiempo
+    alpha: Constante de difusividad
     """
 
     r = dt * alpha / (h^2)
@@ -76,14 +78,6 @@ function metodo_implicito(tf, h, dt, alpha)
     return hcat(zeros(n), U, zeros(n))
 end
 
-
-
-
-
-
-
-
-
 function _construir_matriz_para_metodo_implicito(n, r)
     """
     Recibe
@@ -96,4 +90,90 @@ function _construir_matriz_para_metodo_implicito(n, r)
     tridiag_matrix = Tridiagonal(off_diag, main_diag, off_diag)
 
     return tridiag_matrix
+end
+
+function _construir_matriz_llena_para_metodo_implicito_2d(n, m, r_x, r_y)
+    """
+    Recibe
+
+    n: cantidad de pasos espaciales interiores en x
+    m: cantidad de pasos espaciales interiores en y
+    r_x: coeficiente calculado como  dt * alpha / (h_x^2)
+    r_y: coeficiente calculado como  dt * alpha / (h_y^2)
+    """
+
+    N = n*m
+    beta = 1 + 2*r_x + 2*r_y
+
+    # Matriz diagonal
+    main_diag = fill(beta, n)
+    off_diag = fill(-r_x, n-1)
+    main_diag_matrix = Tridiagonal(off_diag, main_diag, off_diag)
+
+    # Matriz subdiagonal
+    main_diag = fill(-r_y, n)
+    subdiagonal_matrix = diagm(main_diag)
+
+    main_matrix = zeros(N, N)
+
+    for i in 1:n:N-1
+        for j in 1:n:N-1
+            if i == j
+                main_matrix[i : i+n-1, j : j+n-1] = main_diag_matrix
+            elseif abs(i-j) == n
+                main_matrix[i : i+n-1, j : j+n-1] = subdiagonal_matrix
+            else
+                main_matrix[i : i+n-1, j : j+n-1] = zeros(n, n)
+            end
+        end
+    end
+
+    return main_matrix
+end
+
+#Preguntar Igna como corregir N y M
+function _inital_heat_2d(n, m)
+    M = zeros(n, m)
+    M[(n÷3 + 1):2*(n÷3), (m÷3 + 1):2*(m÷3)] = 2*ones(n÷3, m÷3)
+
+    return M
+end
+
+#Cual es la g(x)?
+function metodo_implicito_2d(tf, h_x, h_y, dt, alpha)
+    """
+    Recibe
+
+    tf: Tiempo final
+    h_x: Paso en la dimension x
+    h_y: Paso en la dimension y
+    dt: Paso en el tiempo
+    alpha: Constante de difusividad
+    """
+
+    r_x = dt * alpha / (h_x^2)
+    r_y = dt * alpha / (h_y^2)
+    n = Int(tf ÷ dt)
+    m_x = Int((1 - 2*h_x) ÷ h_x) + 1
+    m_y = Int((1 - 2*h_y) ÷ h_y) + 1
+    M = _construir_matriz_llena_para_metodo_implicito_2d(m_x, m_y, r_x, r_y)
+    initial_heat = reshape(_inital_heat_2d(m_x, m_y), :)
+
+    U = zeros(n, m_x * m_y)
+    U[1, :] = initial_heat
+    descM = lu(M)
+
+    for i in 1:n-1
+        U[i+1, :] .= descM \ U[i, :]
+    end
+
+    U_definitiva = zeros(n, m_x + 2, m_y + 2)
+
+    for i in 1:n
+        #Preguntar a Igna
+        time_state = reshape(U[i, :], (m_x, m_y))
+        U_definitiva[i, 2:m_x+1, 2:m_y+1] .= time_state
+    end
+
+    return U_definitiva
 end
--- TP3/src/notebook.jl
+++ TP3/src/notebook.jl
@@ -38,6 +38,28 @@ end
 # ╔═╡ d30b2b48-7f0c-40f7-8bd0-b908f4d16aed
 gif(anim, "graph_animation.gif", fps = 2)
 
+# ╔═╡ cc8ea44b-60aa-4e3b-80f0-9e161f77923a
+begin
+	h_x = 0.1
+	h_y = 0.2
+	dt_2 = 0.05
+	alpha_2 = (1/2) * (h_x^2 + h_y^2) / dt
+end
+
+# ╔═╡ 1ca37473-919c-4be5-bbde-abe6b79ab3fd
+U_2d_filled_matrix = metodo_implicito_2d(1, h_x, h_y, dt_2, alpha_2)
+
+# ╔═╡ 0cc505a1-832b-40b8-b9a2-2039c080a1d1
+begin
+	n, m_x, m_y = size(U_2d_filled_matrix)
+	anim_2d = @animate for i in 1:n
+		heatmap(U_2d_filled_matrix[i, :, :], clim=(0, 2))
+	end
+end
+
+# ╔═╡ 8f7865b1-e36d-4f4d-9f36-c76b9d4ac3e8
+gif(anim_2d, "graph_animation-2d.gif", fps = 2)
+
 # ╔═╡ Cell order:
 # ╠═c8561f88-916c-11ee-19bb-850b3ffa37d8
 # ╠═6d7e7e0e-e6e4-4fa8-808b-1ec500a4495d
@@ -45,3 +67,7 @@ gif(anim, "graph_animation.gif", fps = 2)
 # ╠═d957c5cf-cacf-42d6-8dca-ecd5cb25e42e
 # ╠═5e150598-7fe8-4e82-a6d8-b1f833dc4569
 # ╠═d30b2b48-7f0c-40f7-8bd0-b908f4d16aed
+# ╠═cc8ea44b-60aa-4e3b-80f0-9e161f77923a
+# ╠═1ca37473-919c-4be5-bbde-abe6b79ab3fd
+# ╠═0cc505a1-832b-40b8-b9a2-2039c080a1d1
+# ╠═8f7865b1-e36d-4f4d-9f36-c76b9d4ac3e8
