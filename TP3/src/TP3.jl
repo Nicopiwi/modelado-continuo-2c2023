cbe0342c7cc3d6a1372d75c7ad135fb75da0a94d
Nicopiwi <nikorozenberg@gmail.com>
2023-12-10 01:04:06 -0300

    emprolijando cosas

--- TP3/src/TP3.jl
+++ TP3/src/TP3.jl
@@ -1,13 +1,33 @@
+"""
+Trabajo Práctico Nº3: Ecuación del Calor
+
+Integrantes:
+
+Mateo Suffern
+Mateo Cesaroni
+Nicolás Ian Rozenberg
+
+En `TP3.jl` se encuentran todas la funciones de servicio utilizadas para resolver las consignas.
+La resolución de las consignas se encuentra en el Pluto Notebook `notebook.jl`
+
+"""
+
+
 using LinearAlgebra
 using SparseArrays
 
 
-function _construir_matriz_para_metodo_explicito(n, r)
+function _construir_matriz_para_metodo_explicito_1d(n, r)
     """
     Recibe
 
-    n: tamaño de la matriz
+    n: Cantidad de pasos interiores en el espacio
     r: coeficiente calculado como  dt * alpha / (h^2)
+
+    Devuelve
+
+    Matriz del método explícito para resolver la ecuación del calor en una dimensión espacial,
+    de tamaño n x n.
     """
     main_diag = fill(1 - 2r, n)
     off_diag = fill(r, n-1)
@@ -19,7 +39,8 @@ end
 function metodo_explicito(tf, n, m, alpha)
     """
     Resuelve la ecuación del calor homogénea unidimensional con condiciones de contorno Dirichlet 
-    nulas en [0, 1] y condición inicial g(x) = x si x ∉ {0, 1}, g(0) = g(1) = 0
+    nulas en [0, 1] y condición inicial g(x) = x si x ∉ {0, 1}, g(0) = g(1) = 0, mediante el método
+    de diferencias finitas explícito.
 
     Recibe
 
@@ -29,13 +50,13 @@ function metodo_explicito(tf, n, m, alpha)
     alpha: Constante de difusividad
 
     Devuelve
-    U: Solución discretizada. U[i, j] representa la solución en el paso i de tiempo, en el paso j del espacio.
+    U: Solución discretizada de tamaño n x (m+2). U[i, j] representa la solución en el paso i de tiempo, en el paso j del espacio.
     """
 
     dt = tf / n
     h = 1 / (m + 1)
     r = dt * alpha / (h^2)
-    M = _construir_matriz_para_metodo_explicito(m, r)  # aca va un m y en la otra un n
+    M = _construir_matriz_para_metodo_explicito_1d(m, r)  # aca va un m y en la otra un n
     U = zeros(n, m)
 
     for i in 1:n
@@ -49,12 +70,31 @@ function metodo_explicito(tf, n, m, alpha)
     return hcat(zeros(n), U, zeros(n))
 end
 
+function _construir_matriz_para_metodo_implicito_1d(n, r)
+    """
+    Recibe
+
+    n: Cantidad de pasos interiores en el espacio
+    r: coeficiente calculado como  dt * alpha / (h^2)
+
+    Devuelve
+
+    Matriz rala del método implícito para resolver la ecuación del calor en una dimensión espacial,
+    de tamaño n x n.
+    """
+    main_diag = fill(1 + 2r, n)
+    off_diag = fill(-r, n-1)
+    tridiag_matrix = spdiagm(-1 => off_diag, 0 => main_diag, 1 => off_diag)
+
+    return tridiag_matrix
+end
 
 function metodo_implicito(tf, n, m, alpha)
     """
 
     Resuelve la ecuación del calor homogénea unidimensional con condiciones de contorno Dirichlet 
-    nulas en [0, 1] y condición inicial g(x) = x si x ∉ {0, 1}, g(0) = g(1) = 0
+    nulas en [0, 1] y condición inicial g(x) = x si x ∉ {0, 1}, g(0) = g(1) = 0, mediante el método
+    de diferencias finitas implícito.
 
     Recibe
 
@@ -64,13 +104,13 @@ function metodo_implicito(tf, n, m, alpha)
     alpha: Constante de difusividad
 
     Devuelve
-    U: Solución discretizada. U[i, j] representa la solución en el paso i de tiempo, en el paso j del espacio.
+    U: Solución discretizada de tamalo n x (m+2). U[i, j] representa la solución en el paso i de tiempo, en el paso j del espacio.
     """
 
     dt = tf / n
     h = 1 / (m + 1)
     r = dt * alpha / (h^2)
-    M = _construir_matriz_para_metodo_implicito(m, r)
+    M = _construir_matriz_para_metodo_implicito_1d(m, r)
     U = zeros(n, m)
     descM = lu(M)
 
@@ -85,19 +125,6 @@ function metodo_implicito(tf, n, m, alpha)
     return hcat(zeros(n), U, zeros(n))
 end
 
-function _construir_matriz_para_metodo_implicito(n, r)
-    """
-    Recibe
-
-    n: tamaño de la matriz
-    r: coeficiente calculado como  dt * alpha / (h^2)
-    """
-    main_diag = fill(1 + 2r, n)
-    off_diag = fill(-r, n-1)
-    tridiag_matrix = spdiagm(-1 => off_diag, 0 => main_diag, 1 => off_diag)
-
-    return tridiag_matrix
-end
 
 function _construir_matriz_llena_para_metodo_implicito_2d(n, m, r_x, r_y)
     """
@@ -107,6 +134,11 @@ function _construir_matriz_llena_para_metodo_implicito_2d(n, m, r_x, r_y)
     m: cantidad de pasos espaciales interiores en y
     r_x: coeficiente calculado como  dt * alpha / (h_x^2)
     r_y: coeficiente calculado como  dt * alpha / (h_y^2)
+
+    Devuelve:
+
+    Matriz de transición de tamaño nm x nm ordinaria para el método implícito para resolver
+    la ecuación del calor bidimensional.
     """
 
     N = n*m
@@ -146,6 +178,11 @@ function _construir_matriz_rala_para_metodo_implicito_2d(n, m, r_x, r_y)
     m: cantidad de pasos espaciales interiores en y
     r_x: coeficiente calculado como  dt * alpha / (h_x^2)
     r_y: coeficiente calculado como  dt * alpha / (h_y^2)
+
+    Devuelve:
+
+    Matriz de transición de tamaño nm x nm rala para el método implícito para resolver
+    la ecuación del calor bidimensional.
     """
     N = n * m
     beta = 1 + 2 * r_x + 2 * r_y
@@ -181,8 +218,12 @@ function _inital_heat_2d(n, m)
     return M
 end
 
-function metodo_implicito_2d(tf, steps_space_x, steps_space_y, n, alpha; llena=false, LU=true)
+function metodo_implicito_2d(tf, steps_space_x, steps_space_y, n, alpha, condicion_inicial; llena=false, LU=true)
     """
+    Resuelve la ecuación del calor homogénea unidimensional con condiciones de contorno Dirichlet 
+    nulas en [0, 1] x [0, 1] y condiciones iniciales pasadas por parámetro, mediante el método
+    de diferencias finitas implícito.
+
     Recibe
 
     tf: Tiempo final
@@ -190,8 +231,15 @@ function metodo_implicito_2d(tf, steps_space_x, steps_space_y, n, alpha; llena=f
     steps_space_y: Cantidad de pasos interiores en la dimension y
     n: Cantidad de pasos en el tiempo
     alpha: Constante de difusividad
-    llena = true o false (en caso de falso es Rala)
-    LU = true o false (se debe precalcular descomposicion LU)
+    condicion_inicial: Matriz de steps_space_x x steps_space_y. Su transpuesta indexada en (x, y) representativa
+    el calor en la posición x, y.
+    llena = Si la matriz del método debe ser llena (matriz ordinaria). Sino, es rala (Construida mediante SparseArray)
+    LU = Si se debe precalcular descomposicion LU de la matriz del método
+
+    Devuelve
+
+    U: Solución discretizada de tamalo n x (steps_space_x+2) x (steps_space_y+2). U[i, x, y] representa la solución en el paso i de tiempo,
+    en la esquina (x, y) que representa al espacio.
     """
 
     dt = tf / n
@@ -205,10 +253,9 @@ function metodo_implicito_2d(tf, steps_space_x, steps_space_y, n, alpha; llena=f
     else
         M = _construir_matriz_rala_para_metodo_implicito_2d(steps_space_x, steps_space_y, r_x, r_y)
     end
-    initial_heat = _inital_heat_2d(steps_space_x, steps_space_y)[:]
 
     U = zeros(n, steps_space_x * steps_space_y)
-    U[1, :] = initial_heat
+    U[1, :] = condicion_inicial[:]
     
     if LU
         descM = lu(M)
@@ -220,11 +267,11 @@ function metodo_implicito_2d(tf, steps_space_x, steps_space_y, n, alpha; llena=f
         U[i+1, :] .= descM \ U[i, :]
     end
 
-    U_definitiva = zeros(n, steps_space_x + 2, steps_space_y + 2)
+    U_definitiva = zeros(n, steps_space_y + 2, steps_space_x + 2)
 
     for i in 1:n
-        time_state = reshape(U[i, :], (steps_space_x, steps_space_y))
-        U_definitiva[i, 2:steps_space_x+1, 2:steps_space_y+1] .= time_state
+        time_state = transpose(reshape(U[i, :], (steps_space_x, steps_space_y)))
+        U_definitiva[i, 2:steps_space_y+1, 2:steps_space_x+1] .= time_state
     end
 
     return U_definitiva
@@ -238,6 +285,11 @@ function _construir_matriz_rala_para_difusion_transporte(n, r, s)
     n: cantidad de pasos espaciales en cada dimension
     r: coeficiente calculado como  dt * alpha / (h ^ 2)
     s: coeficiente calculado como dt * beta / 2 * h
+
+    Devuelve:
+
+    Matriz de transición de tamaño (n+1)n x (n+1)n rala para el método implícito para resolver
+    la ecuación de difusión con transporte bidimensional con condiciones Neumann nulas.
     """
     N = (n + 1) * n
 
@@ -293,8 +345,12 @@ function _condicion_inicial_transporte(n, m)
 end
 
 
-function metodo_implicito_problema_transporte_2d(tf, steps_space, n, alpha, beta)
+function metodo_implicito_problema_transporte_2d(tf, steps_space, n, alpha, beta, condicion_inicial)
     """
+    Resuelve la ecuación de difusión con transporte con condiciones de contorno Neumann 
+    nulas en [0, 1] x [0, 1] y condiciones iniciales pasadas por parámetro, mediante el método
+    de diferencias finitas implícito.
+
     Recibe
 
     tf: Tiempo final
@@ -302,6 +358,13 @@ function metodo_implicito_problema_transporte_2d(tf, steps_space, n, alpha, beta
     n: Cantidad de pasos en el tiempo
     alpha: Constante de difusividad
     beta: Constante de transporte
+    condicion_inicial: Matriz de (steps_space + 1) x steps_space. Su transpuesta indexada en (x, y) representativa
+    el calor en la posición x, y.
+
+    Devuelve
+
+    U: Solución discretizada de tamalo n x (steps_space+2) x (steps_space+2). U[i, x, y] representa la solución en el paso i de tiempo, 
+    en la esquina (x, y) que representa al espacio.
     """
 
     dt = tf / n
@@ -310,7 +373,7 @@ function metodo_implicito_problema_transporte_2d(tf, steps_space, n, alpha, beta
     s = beta * dt / (2 * h)
 
     M = _construir_matriz_rala_para_difusion_transporte(steps_space, r, s)
-    initial_heat = _condicion_inicial_transporte(steps_space + 1, steps_space)[:]
+    initial_heat = condicion_inicial[:]
 
     U = zeros(n, (steps_space + 1) * steps_space)
     U[1, :] = initial_heat
@@ -323,11 +386,11 @@ function metodo_implicito_problema_transporte_2d(tf, steps_space, n, alpha, beta
     U_definitiva = zeros(n, steps_space + 2, steps_space + 2)
 
     for i in 1:n
-        time_state = reshape(U[i, :], (steps_space + 1, steps_space))
-        U_definitiva[i, 1:steps_space+1, 1:steps_space] .= time_state
+        time_state = transpose(reshape(U[i, :], (steps_space + 1, steps_space)))
+        U_definitiva[i, 1:steps_space, 1:steps_space+1] .= time_state
 
-        # Copiamos la primera columna por simetría
-        U_definitiva[i, 1:steps_space+1, steps_space+1] = U_definitiva[i, 1:steps_space+1, 1]
+        # Copiamos la primera fila por simetría
+        U_definitiva[i, steps_space+1, 1:steps_space+1] = U_definitiva[i, 1, 1:steps_space+1]
     end
 
     return U_definitiva
--- TP3/src/notebook.jl
+++ TP3/src/notebook.jl
@@ -21,7 +21,7 @@ end
 
 # ╔═╡ 291b3ea8-163e-44d8-85d8-8ec02189206b
 md""" 
-## Método Explícito 1D
+## Método Explícito unidimensional
 """
 
 # ╔═╡ 2ed1733a-a4a1-4f5f-9757-20206decd63c
@@ -48,7 +48,12 @@ end
 anim_explicito = probar_metodo_explicito_1d()
 
 # ╔═╡ d30b2b48-7f0c-40f7-8bd0-b908f4d16aed
-gif(anim_explicito, "graph_animation_metodo_explicito_1d.gif", fps = 50)
+gif(anim_explicito, "graph_animation_metodo_explicito_1d.gif", fps = 30)
+
+# ╔═╡ 09d3253c-c995-4ecd-a4cb-328807b35628
+md""" 
+## Método Implícito Unidimensional
+"""
 
 # ╔═╡ 36605cf4-9350-4380-acfd-3e15102097af
 function probar_metodo_implicito1d()
@@ -77,10 +82,13 @@ anim_implicito = probar_metodo_implicito1d()
 end
 
 # ╔═╡ 793e7a95-f58a-484a-a8a0-974f65d53066
-gif(anim_implicito, "graph_animation_metodo_explicito_1d.gif", fps = 50)
+gif(anim_implicito, "graph_animation_metodo_explicito_1d.gif", fps = 30)
 
 # ╔═╡ ecb1e3d2-6d4d-4fb8-9b2d-30ffe6c9e071
 md""" ## Prueba de estabilidad"""
+
+# ╔═╡ 65dd1d1d-5a7d-4a59-a3bd-ff74bc2f9059
+md"""Mostramos un caso en el que el método explícito comienza a poseer problemas de inestabilidad a diferencia del método implícito."""
 
 # ╔═╡ 8547e827-7fab-492d-b5c2-cd3502e6632d
 function prueba_estabilidad(ϵ)
@@ -88,15 +96,15 @@ function prueba_estabilidad(ϵ)
 	m = 30
 	dt = 1/n
 	h = 1/(m+1)
-	alpha = (1/2) * h^2 / dt # siempre valen 1/2
-	alpha = ϵ * alpha
+	alpha = (1/2) * h^2 / dt 
+	alpha = ϵ * alpha # Lo que implica que r= (1/2) * ϵ
 	sols = [
 		metodo_implicito(1, n, m, alpha),
-		metodo_explicito(1,n,m,alpha)
+		metodo_explicito(1, n, m, alpha)
 	]
 	anim = @animate for i in 1:n
-		plot(0:h:1, sols[1][i, :], ylim=(0, 1), label="metodo_implicito"),
-		plot!(0:h:1, sols[2][i, :], ylim=(0, 1), label="metodo_explicito")
+		plot(0:h:1, sols[1][i, :], ylim=(0, 1), label="Método implícito"),
+		plot!(0:h:1, sols[2][i, :], ylim=(0, 1), label="Método explícito")
 	end
 end
 
@@ -104,9 +112,54 @@ end
 # Elegimos epsilon = 1.04 pero para valores mas grandes que 1 se vuelve inestable
 gif(prueba_estabilidad(1.04), "graph_animation_metodo_implicito_1d.gif", fps = 50)
 
+# ╔═╡ 637558cd-b5ed-4e8b-b01c-976949362c3f
+md"""
+## Resolución de la ecuación del calor bidimensional
+"""
+
+# ╔═╡ b9f4f173-a042-4513-97bc-b4a8a33dda56
+md"""
+### Definición de condiciones iniciales
+
+Hemos definido dos condiciones iniciales bidimensionales para los puntos que quedan. Una rectangular, y la otra circular.
+"""
+
+# ╔═╡ 342f2cf3-0608-4bd8-8634-bd06e983d2b1
+# Crea un rectangulo centrado en el centro del espacio, de tamaño n/3 x m/3
+function _condicion_inicial_rectangular(n, m)
+    M = zeros(n, m)
+    M[(n÷3 + 1):2*(n÷3), (m÷3 + 1):2*(m÷3)] = 2*ones(n÷3, m÷3)
+
+    return M
+end
+
+# ╔═╡ f600dfd8-8bbc-4935-889a-e5249f187a8e
+# Crea un círculo discretizado centrado en el centro del espacio, de radio min(n, m)/4
+# Utilizada para la ecuación de difusión con transporte
+function _condicion_inicial_circular(n, m)
+    matriz = zeros(n, m)
+    centro_n, centro_m = cld(n, 2), cld(m, 2)
+    radio = min(cld(n, 4), cld(m, 4))
+
+    for i in 1:n
+        for j in 1:m
+            if (i - centro_n)^2 + (j - centro_m)^2 <= radio^2
+                matriz[i, j] = 1
+            end
+        end
+    end
+
+    return matriz
+end
+
 # ╔═╡ b025bd94-b97e-4950-a51e-b86efd4067c1
 md"""
-## Comparación de tiempos de ejecución para la resolución de la ecuación de calor en 2D 
+### Comparación de tiempos de ejecución para la resolución de la ecuación de calor en 2D 
+"""
+
+# ╔═╡ df5a91b3-9f78-4be5-b1e9-54c1306f1067
+md"""
+Para el caso bidimensional, se han aplicado dos mejoras de eficiencia principalmente: Construcción de SparseArrays (matriz rala) para representar la matriz del método, y el precálculo de la descomposición LU. Verificamos que dichas mejoras sean efectivas comparando el tiempo de ejecución.
 """
 
 # ╔═╡ b258f0ca-036a-4013-867c-820936878fc5
@@ -116,21 +169,22 @@ function comparar_tiempos_ejecucion_ecuacion_calor()
 	n = 20
 	dt = 1/n
 	alpha = (1/2) * ((1/steps_x)^2 + (1/steps_y)^2) / dt
+	condicion_inicial = _condicion_inicial_rectangular(steps_x, steps_y)
 	
 	println("Tiempo de ejecución del cálculo de solucion utilizando una matriz rala, y precalculando descomposición LU")
-	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, llena=false, LU=true)
+	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, $condicion_inicial, llena=false, LU=true)
 	println(ex_time)
 
 	println("Tiempo de ejecución del cálculo de solucion utilizando una matriz rala, sin precalcular descomposición LU")
-	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, llena=false, LU=false)
+	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, $condicion_inicial, llena=false, LU=false)
 	println(ex_time)
 
 	println("Tiempo de ejecución del cálculo de solucion utilizando una matriz llena, y precalculando descomposición LU")
-	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, llena=true, LU=true)
+	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, $condicion_inicial, llena=true, LU=true)
 	println(ex_time)
 
 	println("Tiempo de ejecución del cálculo de solucion utilizando una matriz llena, sin precalcular descomposición LU")
-	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, llena=true, LU=false)
+	ex_time, _ = @btime metodo_implicito_2d(1, $steps_x, $steps_y, $n, $alpha, $condicion_inicial, llena=true, LU=false)
 	println(ex_time)
 	
 end
@@ -138,21 +192,29 @@ end
 # ╔═╡ 9802ff27-f4a2-4def-a9b2-96881842ef14
 comparar_tiempos_ejecucion_ecuacion_calor()
 
+# ╔═╡ 01a74b17-4f54-46e2-9ecf-167c24be20b2
+md"""
+## Resolución de la ecuación del calor bidimensional
+"""
+
 # ╔═╡ 128ea921-46e2-413b-b462-f8539de3ed66
 function probar_metodo_implicito_ecuacion_calor_2d()
 	steps_x = 80
 	steps_y = 100
-	n = 150
+	n = 400
 	dt = 1/n
+	condicion_inicial = _condicion_inicial_rectangular(steps_x, steps_y)
 
 	# r = 1/2
 	alpha = (1/2) * ((1/steps_x)^2 + (1/steps_y)^2) / dt
 	
-	U = metodo_implicito_2d(1, steps_x, steps_y, n, alpha, llena=false, LU=true)
+	U = metodo_implicito_2d(1, steps_x, steps_y, n, alpha, condicion_inicial, llena=false, LU=true)
 	anim_2d = @animate for i in 1:n
-		heatmap(transpose(U[i, :, :]), clim=(0,2),
-			xticks=(1:steps_x, [string(round(j, digits=3)) for j in 0:(1/steps_x-1):1]), 
-			yticks=(1:steps_y, [string(round(j, digits=3)) for j in 0:(1/steps_y-1):1])
+		heatmap(
+			0:1/(steps_x+2):1,
+			0:1/(steps_y+2):1,
+			U[i, :, :], 
+			clim=(0,2),
 		)
 	end
 
@@ -163,7 +225,7 @@ end
 anim_2d = probar_metodo_implicito_ecuacion_calor_2d()
 
 # ╔═╡ 8f7865b1-e36d-4f4d-9f36-c76b9d4ac3e8
-gif(anim_2d, "graph_animation-2d.gif", fps = 20)
+gif(anim_2d, "graph_animation-2d.gif", fps = 50)
 
 # ╔═╡ e3e0b706-4fd8-4782-a15a-7a88ad104070
 md"""
@@ -175,15 +237,20 @@ function probar_metodo_implicito_difusion_con_transporte_2d()
 	steps_space = 80
 	n = 100
 	dt = 1/n
+	condicion_inicial = _condicion_inicial_circular(steps_space + 1, steps_space)
 
 	# r = 1/2
 	alpha = (1/2) * (2/steps_space)^2 / dt
 	beta = 2
 	
-	U = metodo_implicito_problema_transporte_2d(1, steps_space, n, alpha, beta)
+	U = metodo_implicito_problema_transporte_2d(1, steps_space, n, alpha, beta, condicion_inicial)
 	anim_2d = @animate for i in 1:n
-		heatmap(transpose(U[i, :, :]), clim=(0,1),
-	)
+		heatmap(
+			0:1/(steps_space+2):1,
+			0:1/(steps_space+2):1,
+			U[i, :, :], 
+			clim=(0,1),
+		)
 	end
 
 	return anim_2d
@@ -202,15 +269,23 @@ gif(anim_2d_transport, "graph_animation-transport-2d.gif", fps = 10)
 # ╠═2ed1733a-a4a1-4f5f-9757-20206decd63c
 # ╠═58e315d2-4645-4376-80b7-19d6d2d31517
 # ╠═d30b2b48-7f0c-40f7-8bd0-b908f4d16aed
+# ╟─09d3253c-c995-4ecd-a4cb-328807b35628
 # ╠═36605cf4-9350-4380-acfd-3e15102097af
 # ╠═c9046b3d-1019-4b99-b3d5-a3aa81df469b
 # ╠═793e7a95-f58a-484a-a8a0-974f65d53066
 # ╟─ecb1e3d2-6d4d-4fb8-9b2d-30ffe6c9e071
+# ╟─65dd1d1d-5a7d-4a59-a3bd-ff74bc2f9059
 # ╠═8547e827-7fab-492d-b5c2-cd3502e6632d
 # ╠═98eed8e8-4631-4f8b-a335-7d1ccd4873e1
+# ╟─637558cd-b5ed-4e8b-b01c-976949362c3f
+# ╟─b9f4f173-a042-4513-97bc-b4a8a33dda56
+# ╠═342f2cf3-0608-4bd8-8634-bd06e983d2b1
+# ╠═f600dfd8-8bbc-4935-889a-e5249f187a8e
 # ╟─b025bd94-b97e-4950-a51e-b86efd4067c1
+# ╟─df5a91b3-9f78-4be5-b1e9-54c1306f1067
 # ╠═b258f0ca-036a-4013-867c-820936878fc5
 # ╠═9802ff27-f4a2-4def-a9b2-96881842ef14
+# ╟─01a74b17-4f54-46e2-9ecf-167c24be20b2
 # ╠═128ea921-46e2-413b-b462-f8539de3ed66
 # ╠═db108402-c5ce-4b38-be60-3c3a7521fcd0
 # ╠═8f7865b1-e36d-4f4d-9f36-c76b9d4ac3e8
